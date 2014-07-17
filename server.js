edd12a23505438439579f93bbeba42bfbdf5eedc
Zong-Ying Lyu <zongyinglyu@dopod.com>
2014-07-17 13:54:28 +0800

    1.update express 3.x to 4.x

--- server.js
+++ server.js
@@ -4,31 +4,27 @@ var expressValidator = require('express-validator');
 
 exports.start = function (config) {
     var app = express();
-    var access_logfile = fs.createWriteStream('./log/access.log', {flags: 'a'});
+    //var access_logfile = fs.createWriteStream('./log/access.log', {flags: 'a'});
+    //app.use(express.cookieParser());
+    //app.use(express.bodyParser());
+    //app.use(expressValidator);
+    app.use(express.static( __dirname + '/htdocs'));    
+    //app.use(express.logger({stream: access_logfile, immediate: true}));
 
+    /*
+    //web basic auth
+    app.use(express.basicAuth(function(user, pass) {
+        return user === 'testUser' && pass === 'testPass';
+    }));
+    */
 
-    app.configure(function() {
-        app.use(express.bodyParser());
-        //app.use(expressValidator);
-        app.use(express.static( __dirname + '/htdocs'));
-        app.use(app.router);
-        app.use(express.logger({stream: access_logfile, immediate: true}));
-        /*
-        //web basic auth
-        app.use(express.basicAuth(function(user, pass) {
-            return user === 'testUser' && pass === 'testPass';
-        }));
-        */
-
-    });
-
-    app.configure('development', function() {
-        app.use(express.errorHandler({dumpExceptions: true, showStack: true}));
-    });
-
-    app.configure('production', function() {
-        app.use(express.errorHandler());
-    });
+    var env = process.env.NODE_ENV || 'development';
+    if ('development' == env) {
+       //app.use(express.errorHandler({dumpExceptions: true, showStack: true}));
+    }
+    else {
+       //app.use(express.errorHandler()); 
+    }
 
     require('./router')(app);
 
