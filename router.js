df975e53ca80a6f522de7a9affe453af89544b7b
Zong-Ying Lyu <zongyinglyu@dopod.com>
2014-10-24 14:18:17 +0800

    1. added got POST method request parameter

--- router.js
+++ router.js
@@ -1,10 +1,16 @@
 var async = require('async');
-module.exports = function(app) {
+module.exports = function(app) {    
     //router
-    app.get('/hello', function(req, res){
-        res.send('Hello World');
+    app.get('/get', function (req, res){        
+        res.send('Got a GET request');       
+        console.log(req.query);
     });
-    
+
+    app.post('/post', function (req, res) {
+        res.send('Got a POST request');
+        console.log(req.body);
+    });
+
     app.get('*', notFound);
 };
 
--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 exports.start = function (config) {
     var app = express();
-    
+    var router = express.Router();    
+
     app.use(function(req, res, next){
         console.log('%s %s', req.method, req.url);
         next();
     });
-
-     app.use(express.static( __dirname + '/public'));    
+    
+    app.use(bodyParser.json({ type: 'application/json' }))    
+    app.use(bodyParser.urlencoded({ type: 'application/x-www-form-urlencoded' }))    
+    app.use(express.static( __dirname + '/public'));    
     
     var env = process.env.NODE_ENV || 'development';
+    
     if ('development' == env) {       
     }
     else {       
     }
 
-    require('./router')(app);
+    require('./router')(app, router);
 
     var server = app.listen(config.port, function() {
         console.log('Listening on port %d', server.address().port);
